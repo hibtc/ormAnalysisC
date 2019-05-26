aa6b720bee79476c5694512da362c353a0b87737
Cris Cortes <ecortesg95@gmail.com>
2019-05-26 14:43:38 +0200

    Add the function optimize2(), which accepts a list of parameters to be fitted

--- OrbitResponseMatrix.py
+++ OrbitResponseMatrix.py
@@ -11,6 +11,7 @@ class OrbitResponseMatrix:
         self.madxFile  = madxFile
         self.kickers   = {}
         self.nMonitors = len(monitors)
+        self.nParams   = 0
         """
         We can spare some time if the ORM has already been computed
         to tweak the numerics, note some numerical precision is lost
@@ -25,6 +26,11 @@ class OrbitResponseMatrix:
         self.Mx = 0
         self.My = 0
         self.dp0 = np.zeros(self.nMonitors + self.nKickers)
+        self.dOrmxdP = []
+        self.dOrmydP = []
+        self.pList = []
+        self.dp0List = []
+        self.y0 = []
         self.computeOrm()
 
     def writeMessOrm(self, write=True):
@@ -37,7 +43,6 @@ class OrbitResponseMatrix:
         ormy = []
 
         for measurement in self.messFiles:
-
             ormA = ormAna.OrmAnalysis( measurement, self.madxFile )
             ks =  ormA.kickers
             m  =  ormA.monitor
@@ -55,17 +60,6 @@ class OrbitResponseMatrix:
                 ormx.append([self.monitors[m], i, mMx[i], dmMx[i], mModelx[i]])
                 ormy.append([self.monitors[m], i, mMy[i], dmMy[i], mModely[i]])
 
-            """
-            #Simulated data --> Algorithm works alright!!!
-            for i in range(len(ks)):
-                if i == 2 or i ==3:
-                    ormx.append([self.monitors[m], i, mModelx[i]*1.1, 1e-3, mModelx[i]])
-                    ormy.append([self.monitors[m], i, mModely[i]*1, dmMy[i], mModely[i]])
-                else:
-                    r = 1 + 1*np.random.rand()/100
-                    ormx.append([self.monitors[m], i, mModelx[i]*r, 1e-3, mModelx[i]])
-                    ormy.append([self.monitors[m], i, mModely[i], dmMy[i], mModely[i]])
-            """
         if(write):
             head = 'Monitor  Kicker   orm   dorm   madxOrm'
             np.savetxt('ormx.txt',ormx, fmt=['%i','%i','%e', '%e','%e'],
@@ -114,6 +108,20 @@ class OrbitResponseMatrix:
             My[i][int(Cijy[0])] = -entryCijy
             My[i][self.nMonitors + int(Cijy[1])] = entryCijy
 
+        if(len(self.dOrmxdP)):
+            dOrmx = np.transpose(self.dOrmxdP)
+            dOrmy = np.transpose(self.dOrmydP)
+            for i in range(nColumns):
+                dOrmx[i] = dOrmx[i]/Cijx[3]
+                dOrmy[i] = dOrmy[i]/Cijy[3]
+                """
+                for e in range(len(dOrmx[i])):
+                    if dOrmx[i][e] > 10: dOrmx[i][e] = 0
+                    if dOrmy[i][e] > 10: dOrmy[i][e] = 0
+                """
+            Mx = np.hstack((dOrmx,Mx))
+            My = np.hstack((dOrmy,My))
+
         """
         In case we want to see that everything works fine.
         Difficult to watch though, since we have an ~50x(7+19)
@@ -127,6 +135,39 @@ class OrbitResponseMatrix:
 
         self.Mx = Mx
         self.My = My
+
+    def computedOrmdP(self):
+        """
+        Computes the orbit response matrix derivative
+        """
+        dOrmx = []
+        dOrmy = []
+
+        #Initialize dp0 with zeros
+        if len(self.dp0List) == 0: self.dp0List = np.zeros(len(self.pList))
+
+        for measurement in self.messFiles:
+            ormA = ormAna.OrmAnalysis( measurement, self.madxFile )
+            dormx, dormy = ormA.computeDorm(self.pList, self.dp0List)
+            #print(dormx)
+            dOrmx.append(dormx)
+            dOrmy.append(dormy)
+
+        dormx = []
+        dormy = []
+        for j in range( self.nParams ):
+            myArrax = []
+            myArray = []
+            for i in range( len(self.messFiles) ):
+                for k in dOrmx[i][j]:
+                    myArrax.append(k)
+                for k in dOrmy[i][j]:
+                    myArray.append(k)
+            dormx.append(myArrax)
+            dormy.append(myArray)
+
+        self.dOrmxdP = dormx
+        self.dOrmydP = dormy
 
     def fitParameters(self, singularMask=1e-4):
         """
@@ -168,14 +209,40 @@ class OrbitResponseMatrix:
         V = np.transpose(Vt)
         Ut =  np.transpose(U)
         Bplus = np.matmul(V,np.matmul(D,Ut))
-        y = np.hstack((vecOrmx, vecOrmy))
+        self.y0 = np.hstack((vecOrmx, vecOrmy))
         dp0 = np.matmul(Bplus, Mt)
-        self.dp0 = np.matmul(dp0, y)
+        self.dp0 = np.matmul(dp0, self.y0)
 
+        return self.actualizeOrmModel()
+
+    def actualizeOrmModel(self):
         """
-        We scale the monitor reads and the kickers according to
-        Eqs. 8 and 9.
+        This function actualizes the model with the computed
+        parameters stored in dp0.
+        We scale the parameters, monitor reads and the kickers
+        according to Eqs. 8 and 9.
         """
+        cxx = []
+        cyy = []
+        if self.nParams:
+            """
+            We recompute the model with the fitted parameters
+            """
+            for m in self.messFiles:
+                ormA = ormAna.OrmAnalysis( m, self.madxFile )
+                ks =  ormA.kickers
+                m  =  ormA.monitor
+
+                mModelx, mModely = ormA.computeOrmModelpList(self.pList,
+                                                             self.dp0[:self.nParams])
+                for i in range(len(ks)):
+                    cxx.append(mModelx[i])
+                    cyy.append(mModely[i])
+
+            for i in range(len(cxx)):
+                self.ormMx[i][4] = cxx[i]
+                self.ormMy[i][4] = cyy[i]
+
         vecOrmx = self.ormMx
         vecOrmy = self.ormMy
 
@@ -193,18 +260,27 @@ class OrbitResponseMatrix:
         self.ormMy = vecOrmy
         self.computeOrm()
 
-        L2error = np.sqrt(sum(y)**2)
+        vecOrmx = self.ormMx
+        vecOrmy = self.ormMy
+
+        vecOrmx = np.transpose(vecOrmx)
+        vecOrmy = np.transpose(vecOrmy)
+
+        vecOrmx = (vecOrmx[2]-vecOrmx[4])/vecOrmx[3]
+        vecOrmy = (vecOrmy[2]-vecOrmy[4])/vecOrmy[3]
+
+        y1 = np.hstack((vecOrmx, vecOrmy))
+        L2error = np.sqrt(sum((y1-self.y0)**2))
+        self.y0 = y1
 
         return L2error
 
-    def optimize(self, nIterations=100,plot=True, singularMask=1e-4):
+    def optimize(self, plot=True, singularMask=1e-4):
         """
         Computes the optimization scheme with help of the
         function fitParameters and is able to plot the results.
         It prints out the computed fit parameters for the corrector
         kickers and the monitors.
-        @param nIterations is the number of iterations the algorithm
-               will run.
         @param plot is a boolean, if True, the horizontal and vertical
                orbit response will be plotted together with the initial
                MAD-X model and the fitted orbit response.
@@ -246,7 +322,8 @@ class OrbitResponseMatrix:
         kickNames = list(self.kickers.keys())
         monNames  = list(self.monitors.keys())
 
-        for i in range( nIterations ):
+        error = 1
+        while(error > 1e-10):
             monx = self.dp0[:self.nMonitors]
             kickx = self.dp0[self.nMonitors:]
             kickerFits.append(kickx)
@@ -255,13 +332,18 @@ class OrbitResponseMatrix:
             L2.append(error)
 
         kickerFits = np.transpose(kickerFits)
-
+        kFits = []
+        print("Kickers:")
         for i in range(len(kickerFits)):
             print(kickNames[i],' : ',round(sum(kickerFits[i]),4))
+            kFits.append(sum(kickerFits[i]))
 
         monFits = np.transpose(monFits)
+        mFits = []
+        print("Monitors:")
         for i in range(len(monFits)):
             print(monNames[i]," : " ,round(sum(monFits[i]),4))
+            mFits.append(sum(monFits[i]))
 
         Mx = np.transpose(self.ormMx)
         My = np.transpose(self.ormMy)
@@ -278,9 +360,125 @@ class OrbitResponseMatrix:
 
             plt.plot(L2,marker='.')
             plt.xlabel('Iteration number')
-            plt.ylabel(r'$||\vec{y}||_2$ (See Eq. 11 in Appendix)')
-            #plt.yscale('log')
+            plt.ylabel(r'$||\vec{y}_1 - \vec{y}_0||_2$')
+            plt.yscale('log')
             plt.show()
+
+        return np.array(mFits), np.array(kFits)
+
+    def optimize2(self, pList, plot=True, singularMask=1e-4, nIterations=1):
+        """
+        Computes the optimization scheme with help of the
+        function fitParameters and is able to plot the results.
+        It prints out the computed fit parameters for the corrector
+        kickers and the monitors.
+        It also takes into consideration a list of given lattice parameters
+        in the fit.
+        @param plot is a boolean, if True, the horizontal and vertical
+               orbit response will be plotted together with the initial
+               MAD-X model and the fitted orbit response.
+        @param singularMask see function fitParameters()
+        """
+        self.pList = pList
+        self.nParams = len(pList)
+        self.computedOrmdP()
+        self.computeOrm()
+        L2 = []
+
+        kickers = list(self.kickers.keys())
+        xlabels = []
+        monPos = []
+
+        Mx = np.transpose(self.ormMx)
+        My = np.transpose(self.ormMy)
+
+        if(plot):
+            for i in range (len(Mx[1])):
+                xlabels.append(kickers[int(Mx[1][i])])
+                if (Mx[1][i] == 0): monPos.append(i)
+
+            x = np.linspace(0,len(Mx[2]),len(Mx[2]))
+            plt.figure(1)
+            plt.errorbar(x,Mx[2],yerr=Mx[3],marker='.',label='xMeasured')
+            plt.plot(x,Mx[4],label='xModel',marker='.')
+            locs, labels = plt.xticks()
+            plt.xticks(x, xlabels, rotation='vertical')
+            plt.legend(loc=0)
+            for i in monPos:
+                plt.axvline(i,linestyle='--')
+
+            plt.figure(2)
+            plt.errorbar(x,My[2],yerr=Mx[3],marker='.',label='yMeasured')
+            plt.plot(x,My[4],label='yModel',marker='.')
+            plt.legend(loc=0)
+            for i in monPos:
+                plt.axvline(i,linestyle='--')
+
+        kickerFits = []
+        monFits = []
+        kickNames = list(self.kickers.keys())
+        monNames  = list(self.monitors.keys())
+
+        print('Number of fit parameters: ', self.nParams)
+        print('Number of monitors: ', len(monNames))
+        print('Number of kickers: ', len(kickNames))
+        print('Fitting model to data. This might take a while...')
+        error = 10000
+        iteration = 0
+        for iteration in range(43):
+        #while( error > 1e-1 ):
+            print('Iteration ',iteration+1,' , ERROR = ', error)
+            error = self.fitParameters(singularMask)
+            L2.append(error)
+            monx = self.dp0[self.nParams:self.nParams+len(monNames)]
+            kickx = self.dp0[self.nParams+len(monNames):]
+            kickerFits.append(kickx)
+            monFits.append(monx)
+            self.computedOrmdP()
+            self.computeOrm()
+            #iteration += 1
+
+        kickerFits = np.transpose(kickerFits)
+        kFits = []
+        print("Kickers:")
+        for i in range(len(kickerFits)):
+            print(kickNames[i],' : ',round(sum(kickerFits[i]),4))
+            kFits.append(sum(kickerFits[i]))
+        print()
+
+        monFits = np.transpose(monFits)
+        mFits = []
+        print("Monitors:")
+        for i in range(len(monFits)):
+            print(monNames[i]," : " ,round(sum(monFits[i]),4))
+            mFits.append(sum(monFits[i]))
+        print()
+
+        print('Parameters:')
+        for i in range(len(self.pList)):
+            print(self.pList[i], ' : ', round(self.dp0[i],5))
+        print()
+
+        Mx = np.transpose(self.ormMx)
+        My = np.transpose(self.ormMy)
+
+        if(plot):
+            plt.figure(1)
+            plt.plot(x,Mx[4],label='xFit',marker='.')
+            plt.ylabel(r'Horizontal orbit response [mm mrad$^{-1}$]')
+            plt.legend(loc=0)
+            plt.figure(2)
+            plt.plot(x,My[4],label='yFit',marker='.')
+            plt.legend(loc=0)
+            plt.show()
+
+            plt.plot(L2,marker='.')
+            plt.xlabel('Iteration number')
+            plt.ylabel(r'$||\vec{y}_1 - \vec{y}_0||_2$')
+            plt.yscale('log')
+            plt.show()
+
+        return np.array(mFits), np.array(kFits)
 
 def main():
     # Here are the measurements of the first session
@@ -306,16 +504,24 @@ def main():
         '2019-05-12_02-39-53_hht3_h3dg3g.orm_measurement.yml',
         '2019-05-12_02-51-13_hht3_b3dg2g.orm_measurement.yml',
         '2019-05-12_03-05-19_hht3_b3dg3g.orm_measurement.yml',
+        '/home/cristopher/HIT/ormAnalysis/ormMessdata/2019-05-11/GantryDaten/nullDeg/2019-05-12_05-19-01_hht3_g3dg3g.orm_measurement.yml',
+        #'/home/cristopher/HIT/ormAnalysis/ormMessdata/2019-05-11/GantryDaten/nullDeg/2019-05-12_05-00-51_hht3_g3dg5g.orm_measurement.yml'
     ]
 
     for f in range(len(messFiles1)):
         messFiles1[f] = prePath1 + messFiles1[f]
-        messFiles3[f] = prePath3 + messFiles3[f]
+        #messFiles3[f] = prePath3 + messFiles3[f]
 
-    monitors = {'h1dg1g':0, 'h1dg2g':1, 'h2dg2g':2, 'h3dg3g':3,'b3dg2g':4, 'b3dg3g':5}#,'g3dg3g':6,'g3dg5g':7,'t3df1':8}
+    monitors = {'h1dg1g':0, 'h1dg2g':1, 'h2dg2g':2, 'h3dg3g':3,'b3dg2g':4, 'b3dg3g':5}#,'g3dg3g':6}#,'g3dg5g':7,'t3df1':8}
     madxFile = "../ormAnalysis/hit_models/hht3/run.madx"
 
-    orm = OrbitResponseMatrix( messFiles3, monitors, madxFile, readOrm=1 )
-    orm.optimize(nIterations=10, plot=1, singularMask=1e2)
+    #pList = ['kL_H1QD11','kL_H1QD12','kL_H2QT11','kL_H2QT12','kL_H2QT13']
+    #pList = ['kL_H2QT11','kL_H2QT12','kL_H2QT13']
+    pList = ['ay_h1ms1','ax_h1ms2','ax_h1mb1','ax_h1ms3','ay_h1ms4'  ]
+    orm1 = OrbitResponseMatrix( messFiles1, monitors, madxFile, readOrm=1 )
+    #orm1.optimize(plot=1)
+    orm1.optimize2(pList,plot=1,singularMask=5e-6,nIterations=20)
+    #orm2 = OrbitResponseMatrix( messFiles3, monitors, madxFile, readOrm=0 )
+    #m2, k2 = orm2.optimize(plot=0, singularMask=1e-1)
 
 main()
--- OrmAnalysis.py
+++ OrmAnalysis.py
@@ -9,6 +9,8 @@ class OrmAnalysis:
     This Class intends to compute the measured and the modeled orbit
     response matrix and is able to plot the data for control of the
     data quality.
+    It is also able to compute the derivative of the ORM for a given
+    list of parameters.
     @param dataFile is the file path where the measured data is. It is
            expected to be a measurement as produced by madgui in yaml format.
     @param madxModelFile is the file path to the MAD-X model file. The model
@@ -21,6 +23,12 @@ class OrmAnalysis:
         self.data          = self.readData(dataFile)
         self.monitor       = self.getMonitor()
         self.kickers, self.kicks = self.getKicks()
+        # This are initial conditions for the Twiss Module of MAD-X
+        # it doesn't seem to be a strong dependence on them
+        self.dx = 1.0e-4
+        self.dpx = 1.0e-6
+        self.dy = 1.0e-4
+        self.dpy = 1.0e-6
 
     def readData(self, dataFile):
         """
@@ -130,19 +138,8 @@ class OrmAnalysis:
         """
         Computes the orbit response according to the MADX model
         with the same parameters as for a given measurement and
-        for a given monitor and kicker list
-        @param monitor(String) is the name of the monitor for which the
-               orbit response is going to be computed
-        @param kickers(String list) is a list containing the used kickers
-               in the measurement
+        for a given monitor and kicker list.
         """
-        # This are initial conditions for the Twiss Module of MAD-X
-        # it doesn't see to be a strong dependence on them
-        dx = 1.0e-4
-        dpx = 1.0e-6
-        dy = 1.0e-4
-        dpy = 1.0e-6
-
         madx = Madx(stdout=False)
         madx.call(file=self.madxModelFile, chdir=True)
         elems = madx.sequence.hht3.expanded_elements
@@ -156,8 +153,8 @@ class OrmAnalysis:
             twiss1 = madx.twiss(sequence='hht3', RMatrix=True,
                                 alfx = -7.2739282, alfy = -0.23560719,
                                 betx = 44.404075,  bety = 3.8548009,
-                                x = dx, y = dy,
-                                px = dpx, py = dpy)
+                                x = self.dx, y = self.dy,
+                                px = self.dpx, py = self.dpy)
 
             # This should be hopefully zero
             y1 = twiss1.y[iMonitor]
@@ -168,8 +165,8 @@ class OrmAnalysis:
             twiss2 = madx.twiss(sequence='hht3', RMatrix=True,
                                 alfx = -7.2739282, alfy = -0.23560719,
                                 betx = 44.404075,  bety = 3.8548009,
-                                x = dx, y = dy,
-                                px = dpx, py = dpy)
+                                x = self.dx, y = self.dy,
+                                px = self.dpx, py = self.dpy)
 
             y2 = twiss2.y[iMonitor]
             x2 = twiss2.x[iMonitor]
@@ -182,6 +179,96 @@ class OrmAnalysis:
 
         madx.input('STOP;')
         return Cxx, Cxy
+
+    def computeOrmModelpList(self, pList, p0List, kick = 2e-4):
+        """
+        Since there is no method overloading in Python
+        I had to change the name of the method above,
+        but it does the same, but changing the parameters
+        in the model of MAD-X.
+        """
+        madx = Madx(stdout=False)
+        madx.call(file=self.madxModelFile, chdir=True)
+        elems = madx.sequence.hht3.expanded_elements
+        iMonitor = elems.index(self.monitor)
+
+        Cxx = []
+        Cxy = []
+
+        for k in self.kickers:
+
+            madx.globals.update( self.data['model'] )
+            """
+            We update the model given the parameter list
+            before we compute the Orbit Response
+            """
+            for p in range(len(pList)):
+                madx.globals[pList[p]] += p0List[p]
+
+            twiss1 = madx.twiss(sequence='hht3', RMatrix=True,
+                                alfx = -7.2739282, alfy = -0.23560719,
+                                betx = 44.404075,  bety = 3.8548009,
+                                x = self.dx, y = self.dy,
+                                px = self.dpx, py = self.dpy)
+
+            # This should be hopefully zero
+            y1 = twiss1.y[iMonitor]
+            x1 = twiss1.x[iMonitor]
+
+            madx.globals[k] += kick
+
+            twiss2 = madx.twiss(sequence='hht3', RMatrix=True,
+                                alfx = -7.2739282, alfy = -0.23560719,
+                                betx = 44.404075,  bety = 3.8548009,
+                                x = self.dx, y = self.dy,
+                                px = self.dpx, py = self.dpy)
+
+            y2 = twiss2.y[iMonitor]
+            x2 = twiss2.x[iMonitor]
+            # Horizontal and vertical response
+            cx = ( x2 - x1 ) / kick
+            cy = ( y2 - y1 ) / kick
+
+            Cxx.append(cx)
+            Cxy.append(cy)
+
+        return np.array(Cxx), np.array(Cxy)
+
+    def computeDorm(self, pList, p0List, kick = 2e-4, dp = 1e-9):
+        """
+        Computes the orbit response derivative for a given
+        list of parameters.
+        @param pList is the list of parameters the derivative should
+               be computed to
+        @param p0List is the list of the position around the derivative
+               will be computed to
+        @param dp0 cannot be smaller than 1e-9 for numerical precision
+               reasons!!! Going smaller changes drastically the computation.
+               Going up leaves the same results up to numerical precision.
+        """
+        p0List = np.array(p0List)
+        ormMx1, ormMy1 = self.computeOrmModelpList(pList, p0List)
+        dCx = []
+        dCy = []
+        """
+        I had to do this because lists in Python are mutable...
+        I should ask Thomas for a better implementation
+        """
+        for p in range(len(pList)):
+            pList1 = np.ones(len(pList))
+            for q in range(len(pList1)):
+                pList1[q] = p0List[p]
+                if p == q: pList1[q] = p0List[p] + dp
+            #print(p)
+            #print(p0List)
+            #print(pList1)
+            ormMx2, ormMy2 = self.computeOrmModelpList(pList, pList1)
+            dCxdP = (ormMx2 - ormMx1)/dp
+            dCydP = (ormMy2 - ormMy1)/dp
+            dCx.append(dCxdP)
+            dCy.append(dCydP)
+
+        return np.array(dCx), np.array(dCy)
 
     def visualizeData(self, messFile2 = '', saveFigs=False):
         """
